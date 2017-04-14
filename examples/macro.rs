2a3dae2552b1b72bc90df45f1f69c97d2a5d92c5
Donald Whyte <donaldwhyte0@gmail.com>
2017-04-14 12:18:31 +0100

    Support multiple mocked methods in macro

--- examples/macro.rs
+++ examples/macro.rs
@@ -1,7 +1,14 @@
+#![feature(trace_macros)]
+
 extern crate double;
 
-trait Dependency: Clone {
+trait Calculator: Clone {
+    fn multiply(&self, x: i32, y: i32) -> i32;
+}
+
+trait BalanceSheet: Clone {
     fn profit(&self, revenue: u32, costs: u32) -> i32;
+    fn loss(&self, revenue: u32, costs: u32) -> i32;
 }
 
 trait Greeter: Clone {
@@ -9,20 +16,20 @@ trait Greeter: Clone {
 }
 
 macro_rules! mock_trait {
-    ( $mock_name:ident, $($method:ident($retval:ty)($($arg_type:ty),*))* ) => (
+    ($mock_name:ident $(, $method:ident($($arg_type:ty),* ) -> $retval:ty )* ) => (
         #[derive(Debug, Clone)]
         struct $mock_name {
             $(
                 $method: double::Mock<(($($arg_type),*)), $retval>
-            )*
+            ),*
         }
 
         impl Default for $mock_name {
             fn default() -> Self {
                 $mock_name {
                     $(
-                        $method: double::Mock::default(),
-                    )*
+                        $method: double::Mock::default()
+                    ),*
                 }
             }
         }
@@ -30,28 +37,68 @@ macro_rules! mock_trait {
 }
 
 macro_rules! mock_method {
-    ( $method:ident($retval:ty)(&self, $($arg_name:ident: $arg_type:ty),*) ) => (
+    ( $method:ident(&self, $($arg_name:ident: $arg_type:ty),*) -> $retval:ty ) => (
         fn $method(&self, $($arg_name: $arg_type),*) -> $retval {
             self.$method.call(($($arg_name),*))
         }
     );
-    ( $method:ident($retval:ty)(&mut self, $($arg_name:ident: $arg_type:ty),*) ) => (
+    ( $method:ident(&mut self, $($arg_name:ident: $arg_type:ty),*) -> $retval:ty ) => (
         fn $method(&mut self, $($arg_name: $arg_type),*) -> $retval {
+            self.$method.call(($($arg_name),*))
+        }
+    );
+    ( $method:ident-(&self, $($arg_name:ident: $arg_type:ty),*) ) => (
+        fn $method(&self, $($arg_name: $arg_type),*) {
+            self.$method.call(($($arg_name),*))
+        }
+    );
+    ( $method:ident(&mut self, $($arg_name:ident: $arg_type:ty),*) ) => (
+        fn $method(&mut self, $($arg_name: $arg_type),*) {
+            self.$method.call(($($arg_name),*))
+        }
+    );
+    ( $method:ident<($($type_params: tt)*)>(&mut self, $($arg_name:ident: $arg_type:ty),*), {
+        body:block
+    }
+    ) => (
+        fn $method<$($type_params)*>(&mut self, $($arg_name: $arg_type),*) {
             self.$method.call(($($arg_name),*))
         }
     )
 }
 
-mock_trait!(MockDependency, profit(i32)(u32, u32));
-impl Dependency for MockDependency {
-    mock_method!(profit(i32)(&self, revenue: u32, costs: u32));
+trace_macros!(true);
+
+mock_trait!(EmptyMock);
+
+mock_trait!(
+    MockCalculator,
+    multiply(i32, i32) -> i32);
+impl Calculator for MockCalculator {
+    mock_method!(multiply(&self, x: i32, y: i32) -> i32);
 }
 
-mock_trait(MockGreeter, greet(())(String));
+mock_trait!(
+    MockBalanceSheet,
+    profit(u32, u32) -> i32,
+    loss(u32, u32) -> i32);
+impl BalanceSheet for MockBalanceSheet {
+    mock_method!(profit(&self, revenue: u32, costs: u32) -> i32);
+    mock_method!(loss(&self, revenue: u32, costs: u32) -> i32);
+}
+
+mock_trait!(
+    MockGreeter,
+    greet(String) -> ());
+impl Greeter for MockGreeter {
+    mock_method!(greet<(S: AsRef<str>)>(&mut self, name: S), {
+
+    });
+}
 
 fn main() {
     // Test individual return values
-    let mock = MockDependency::default();
+    let mock = MockBalanceSheet::default();
     mock.profit.return_value(42);
     mock.profit.return_value_for((0, 0), 9001);
 
